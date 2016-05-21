e84a93defed3ce28c0647f23a87db1c80395a6d6
Ronnie Bjørkelund <ronnie.bjorkelund@gmail.com>
2016-05-21 05:51:18 +0200

    testing getJSON()

--- js/functions.js
+++ js/functions.js
@@ -1,32 +1,33 @@
 $(document).ready(function(){
 
+
     var fallbackQuotes = { "quotesArray":[
         {
-            "quote":"A banker is a fellow who lends you his umbrella when the sun is shining and wants it back the minute it begins to rain.",
+            "quote":"XA banker is a fellow who lends you his umbrella when the sun is shining and wants it back the minute it begins to rain.",
             "author":"Mark Twain"
         },
         {
-            "quote":"A horse!  A horse!  My kingdom for a horse!",
+            "quote":"XA horse!  A horse!  My kingdom for a horse!",
             "author":"Wm. Shakespeare, \"Richard III\""
         },
         {
-            "quote":"A kind of Batman of contemporary letters.",
+            "quote":"XA kind of Batman of contemporary letters.",
             "author":"Philip Larkin on Anthony Burgess"
         },
         {
-            "quote":"Big book, big bore.",
+            "quote":"XBig book, big bore.",
             "author":"Callimachus"
         },
         {
-            "quote":"Seeing that death, a necessary end, Will come when it will come.",
+            "quote":"XSeeing that death, a necessary end, Will come when it will come.",
             "author":"William Shakespeare, \"Julius Caesar\""
         },
         {
-            "quote":"Something's rotten in the state of Denmark.",
+            "quote":"XSomething's rotten in the state of Denmark.",
             "author":"Shakespeare"
         },
         {
-            "quote":"Sometimes I wonder if I'm in my right mind.  Then it passes off and I'm as intelligent as ever.",
+            "quote":"XSometimes I wonder if I'm in my right mind.  Then it passes off and I'm as intelligent as ever.",
             "author":"Samuel Beckett, \"Endgame\""
         }
     ]};
@@ -34,11 +35,10 @@ $(document).ready(function(){
     var quotes;
     var currentQuote;
 
+    readJSON();
 
     $("button").click(function(){
 
-        readJSON();
-        console.log("qoutes: " + quotes);
         prepeareNextQuote();
         $(".output").html("<p>" + currentQuote.quote + "</p>" + "<p>" + currentQuote.author + "</p>");
 
@@ -47,21 +47,29 @@ $(document).ready(function(){
 
 
     function prepeareNextQuote() {
-        //var randomNum = Math.floor((Math.random() * fallbackQuotes.quotesArray.length));
-        //currentQuote = fallbackQuotes.quotesArray[randomNum];
+
         var randomNum = Math.floor((Math.random() * quotes.quotesArray.length));
         currentQuote = quotes.quotesArray[randomNum];
-        //console.log(randomNum + "/" + quotes.length);
-        //console.log(currentQuote);
+
+    }
+
+    function prepareFallbackQuote() {
+
+        var randomNum = Math.floor((Math.random() * fallbackQuotes.quotesArray.length));
+        currentQuote = fallbackQuotes.quotesArray[randomNum];
+
     }
 
     function readJSON() {
         $.getJSON("js/quotes.json", function(data, status) {
-            quotes = data;
-            console.log("data:" + data.toString());
-            console.log("status:" + status);
+
+            if(status === "success") {
+                quotes = data;
+            } else {
+                prepareFallbackQuote();
+            }
+
         });
-        console.log(quotes);
     }
 
 
